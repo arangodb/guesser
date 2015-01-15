a693b42f43f1425924dfb1602eb64538fb712f17
Andreas Streichardt <andreas.streichardt@gmail.com>
2015-01-15 16:18:53 +0100

    Finalize step7

--- iojs/guesser_server.js
+++ iojs/guesser_server.js
@@ -5,6 +5,25 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 var fs = require("fs");
+var Promise = require("promise");
+var Database = require("arangojs");
+var db = new Database("http://localhost:8529");          // configure server
+var collectionName = "dev_guesser_questions";            // configure collection
+
+var collectionPromise = new Promise(function(resolve, reject) {
+    db.collection(collectionName, false, function(err, res) {
+        if (err) {
+            reject(err);
+        }
+        else {
+            resolve(res);
+        }
+    });
+});
+collectionPromise.then(null, function(err) {
+    console.log("Cannot contact the database! Terminating...");
+    process.exit(1);
+});
 
 ////////////////////////////////////////////////////////////////////////////////
 /// An express app:
@@ -18,6 +37,27 @@ var app = express();
 ////////////////////////////////////////////////////////////////////////////////
 
 app.use(express.static(__dirname + "/static"));
+
+////////////////////////////////////////////////////////////////////////////////
+/// AJAX services:
+////////////////////////////////////////////////////////////////////////////////
+
+app.get("/get/:key", function (req, res) {
+  var key = req.params["key"];
+  collectionPromise.then(function(collection) {
+      collection.document(key, function(err, x) {
+          if (err) {
+              // for production we should implement more sophisticated handling here. Like logging where appropriate etc.
+              res.status(err.code);
+              res.json(err);
+          }
+          else {
+              res.json(x);
+          }
+      });
+  }, null);  // if this were rejected, we would be out already
+});
+
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Now finally make the server:
--- iojs/static/guesser_controller.js
+++ iojs/static/guesser_controller.js
@@ -2,6 +2,36 @@ var app = angular.module("guesser", []);
 
 app.controller("guesserController", function ($scope, $http) {
   $scope.container = {"name":  ""};
-  $scope.view = "welcome";
-});
+  $scope.restart = function () {
+    $http.get("get/root")
+      .success(function(response) {
+                 $scope.current = response;
+               })
+      .error(function(response) {
+               $scope.current = {};
+               alert("AJAX call failed");
+             });
+      $scope.view = "welcome";
+  }
 
+  $scope.restart();
+
+  $scope.update = function () {
+    $scope.theQuestion = $scope.current.question;
+    $scope.theAnswer1 = $scope.current.answer1;
+    $scope.theAnswer2 = $scope.current.answer2;
+    $scope.view = "question";
+  }
+  $scope.startGame = $scope.update;
+
+  $scope.answer = function (newkey) {
+    $http.get("get/"+newkey)
+      .success(function(response) {
+                 $scope.current = response;
+                 $scope.update();
+               })
+      .error(function(response) {
+               alert("AJAX call failed");
+             });
+  } 
+  });
