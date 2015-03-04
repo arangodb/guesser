4de60931032be67d5722587c7e5bb5f7c606325d
Max Neunhoeffer <max@arangodb.com>
2015-03-04 12:11:14 -0800

    Fix routes because of API change.

--- iojs/guesser_server.js
+++ iojs/guesser_server.js
@@ -20,8 +20,9 @@ var app = express();
 
 // leverage NODE_ENV to determine collectionName
 var collectionName = "guesser_questions";            // configure collection
-var putRoute = "put";
+var putRoute = "guesser";
 if (app.get('env') == "development") {
+    putRoute = "dev/" + putRoute;
     collectionName = "dev_" + collectionName;
 }
 
@@ -67,11 +68,11 @@ app.get("/get/:key", function (req, res) {
 });
 
 // This is just a trampoline to the Foxx app:
-var ep = db.route("guesser");
+var ep = db.route(putRoute);
 app.put("/put", function (req, res) {
   req.pipe(concat( function(body) {
     // check out body-parser for a express middleware which handles json automatically
-    ep.put(putRoute, JSON.parse(body.toString()),
+    ep.put("put", JSON.parse(body.toString()),
       function(err, x) {
         if (err) {
           err.error = true;
