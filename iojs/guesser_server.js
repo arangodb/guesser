d88b8a5c8c02c2d74e1caa866c82a15b4be42c22
Max Neunhoeffer <max@arangodb.com>
2015-03-04 12:00:37 -0800

    Adjust for API change in arangojs

--- iojs/guesser_server.js
+++ iojs/guesser_server.js
@@ -60,7 +60,7 @@ app.get("/get/:key", function (req, res) {
 });
 
 // This is just a trampoline to the Foxx app:
-var ep = db.endpoint();
+var ep = db.route("guesser");
 app.put("/put", function (req, res) {
   req.pipe(concat( function(body) {
     // check out body-parser for a express middleware which handles json automatically
